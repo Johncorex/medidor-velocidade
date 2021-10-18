1d25035f1e15204dd1e45532351cd05645491b76
Johncore <32520764+Johncorex@users.noreply.github.com>
2021-10-18 10:18:21 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// velocidade maxima de até 70 km
+// a cada 5 km a cima do limite voce ganha 1 ponto
+// Math.floor ()
+// Se os pontos forem maiores que 12 "Carteira Suspensa"
+
+verificarVelocidade(70);
+
+function verificarVelocidade(velocidade) {
+    if (velocidade <= 70)
+        console.log('Permitido')
+}
